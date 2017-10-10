b40e7ddc58b207a2e8b693d56840f8f040888dc2
Nicolas Fuchs <nicolas.fuchs@edu.hefr.ch>
2017-10-10 14:28:51 +0200

    TP1

--- sockets/tcp/TCPServer.java
+++ sockets/tcp/TCPServer.java
@@ -15,45 +15,45 @@ import java.io.*;
 
 public class TCPServer {
 
-  private static TCPServer instance = null;                 // Instance of Server
-  private static boolean isRunning = true;                  // Variable to stop ServerSocket
-  private static final int SERVER_PORT = 7676;              // Server port to use
-  private static Integer id = 0;                            // Client id
-  private static ServerSocket ss;                           // ServerSocket to accept connections
+    private static TCPServer instance = null;                 // Instance of Server
+    private static boolean isRunning = true;                  // Variable to stop ServerSocket
+    private static final int SERVER_PORT = 7676;              // Server port to use
+    private static Integer id = 0;                            // Client id
+    private static ServerSocket ss;                           // ServerSocket to accept connections
 
-  public static void main(String[] args) {
-    try {
-      instance = new TCPServer();
-      ss = new ServerSocket(SERVER_PORT);
-      launch();
-    } catch (IOException e) {
-      System.out.println("Création du serveur impossible.");
-      e.printStackTrace();
+    public static void main(String[] args) {
+        try {
+            instance = new TCPServer();
+            ss = new ServerSocket(SERVER_PORT);
+            launch();
+        } catch (IOException e) {
+            System.out.println("Création du serveur impossible.");
+            e.printStackTrace();
+        }
     }
-  }
-  
-  private static void launch() {
-    TCPServerThread st;
-    Socket so;
-    log("Listening on TCP Port " + SERVER_PORT + " ...");
-    while(isRunning) {
-      try {
-        so = ss.accept();
-        st = new TCPServerThread(instance, so, id);
-        st.run();
-        log("New connection request from (" + so.getInetAddress().getHostAddress() + ":" + so.getPort() + ")");
-        id++;
-      } catch (IOException e) {
-        e.printStackTrace();
-      }
+
+    private static void launch() {
+        TCPServerThread st;
+        Socket so;
+        log("Listening on TCP Port " + SERVER_PORT + " ...");
+        while(isRunning) {
+            try {
+                so = ss.accept();
+                st = new TCPServerThread(instance, so, id);
+                st.start();
+                log("New connection request from (" + so.getInetAddress().getHostAddress() + ":" + so.getPort() + ")");
+                id++;
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        }
     }
-  }
-  
-  public static void log(Socket so, Integer id, String msg) {
-    System.out.println("Client(" + id + ") from (" + so.getInetAddress().getHostAddress() + ":" + so.getPort() + ") : " + msg);
-  }
-  
-  public static void log(String msg) {
-    System.out.println(msg);
-  }
+
+    public static void log(Socket so, Integer id, String msg) {
+        System.out.println("Client(" + id + ") from (" + so.getInetAddress().getHostAddress() + ":" + so.getPort() + ") : " + msg);
+    }
+
+    public static void log(String msg) {
+        System.out.println(msg);
+    }
 }
--- sockets/tcp/TCPServerThread.java
+++ sockets/tcp/TCPServerThread.java
@@ -13,29 +13,28 @@ package sockets.tcp;
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.io.PrintWriter;
 import java.net.Socket;
 
-public class TCPServerThread implements Runnable {
+public class TCPServerThread extends Thread {
 
-  private TCPServer ts;
-  private Socket so;
-  private Integer id;
-  
-  @Override
-  public void run() {
-    try {
-      BufferedReader in = new BufferedReader(new InputStreamReader(so.getInputStream()));
-      ts.log(so, id, in.readLine());
-    } catch (IOException e) {
-      e.printStackTrace();
+    private TCPServer ts;
+    private Socket so;
+    private Integer id;
+
+    @Override
+    public void run() {
+        try {
+            BufferedReader in = new BufferedReader(new InputStreamReader(so.getInputStream()));
+            ts.log(so, id, in.readLine());
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
     }
-  }
-  
-  public TCPServerThread(TCPServer ts, Socket so, Integer id) {
-    this.ts = ts;
-    this.so = so;
-    this.id = id;
-  }
+
+    public TCPServerThread(TCPServer ts, Socket so, Integer id) {
+        this.ts = ts;
+        this.so = so;
+        this.id = id;
+    }
 
 }
