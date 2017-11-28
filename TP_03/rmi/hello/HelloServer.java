ec454ec9f91a71057dc5288fbaf7e05ec27b4039
Nicolas Fuchs <nicolas.fuchs@edu.hefr.ch>
2017-11-28 13:54:38 +0100

    TP3 hierarchy changes

---
+++ TP_03/rmi/hello/HelloServer.java
@@ -0,0 +1,28 @@
+package rmi.hello;
+
+import java.rmi.*;
+import java.rmi.server.UnicastRemoteObject;
+
+public class HelloServer extends UnicastRemoteObject implements Hello {
+
+    public HelloServer() throws RemoteException {
+        super();
+    }
+
+    public String sayHello() throws RemoteException {
+        System.out.println("- saying hello to all that ask ...");
+        return "Hi Buddy!!";
+    }
+
+    public static void main(String args[]) {
+        try {
+            HelloServer obj = new HelloServer();
+            System.out.println("\nContacting registry on localhost ...");
+            Naming.rebind("//localhost/MyHelloServer", obj);
+            System.out.println("HelloServer bound in registry.\n\nStop server using Ctrl-C.");
+        } catch (Exception e) {
+            System.out.println("HelloServer exception: " + e.getMessage());
+            e.printStackTrace();
+        }
+    }
+}
