aab73f94ffe493dcd4b4f35f47791d6bdc6f11b5
jeangera.pont <jean-gerard.pont@hefr.ch>
2017-12-19 14:10:12 +0100

    add doc

--- TP_03/rmi/hello/HelloServer.java
+++ TP_03/rmi/hello/HelloServer.java
@@ -1,10 +1,13 @@
 package rmi.hello;
 
+import java.io.FileNotFoundException;
+import java.io.PrintWriter;
+import java.io.UnsupportedEncodingException;
 import java.rmi.*;
+import java.rmi.registry.LocateRegistry;
 import java.rmi.server.UnicastRemoteObject;
 
 public class HelloServer extends UnicastRemoteObject implements Hello {
-
     public HelloServer() throws RemoteException {
         super();
     }
@@ -16,6 +19,7 @@ public class HelloServer extends UnicastRemoteObject implements Hello {
 
     public static void main(String args[]) {
         try {
+        	LocateRegistry.createRegistry(1099);
             HelloServer obj = new HelloServer();
             System.out.println("\nContacting registry on localhost ...");
             Naming.rebind("//localhost/MyHelloServer", obj);
